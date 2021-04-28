35fd8426c84435764a5bf9fceae8d34c30d6f2db
Elias Häußler <e.haeussler@familie-redlich.de>
2021-04-28 18:00:07 +0200

    [TASK] Add author company to ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,6 +29,7 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => true,
     'author' => 'Elias Häußler',
     'author_email' => 'elias@haeussler.dev',
+    'author_company' => 'familie redlich digital GmbH',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-11.1.99',
