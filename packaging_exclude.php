e2f293db02da89c20ab3d09e8cee339bdb657f9e
Elias Häußler <elias@haeussler.dev>
2021-05-08 20:25:36 +0200

    [TASK] Exclude all development-only files from being packaged
    Resolves: #1

---
+++ packaging_exclude.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS extension "warming".
+ *
+ * Copyright (C) 2021 Elias Häußler <elias@haeussler.dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+return [
+    'directories' => [
+        '.build',
+        '.git',
+        'bin',
+        'build',
+        'public',
+        'tailor-version-upload',
+        'tests',
+        'vendor',
+    ],
+    'files' => [
+        'DS_Store',
+        'composer.lock',
+        'crowdin.yaml',
+        'editorconfig',
+        'gitattributes',
+        'gitignore',
+        'gitlab-ci.yml',
+        'packaging_exclude.php',
+        'php_cs',
+        'phpstan.neon',
+        'phpunit.coverage.xml',
+        'phpunit.xml',
+    ],
+];
