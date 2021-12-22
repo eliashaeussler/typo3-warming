303b99ae80b9b371a0dc0402585d5d515e42d3b6
Elias Häußler <e.haeussler@familie-redlich.de>
2021-12-22 17:31:33 +0100

    [TASK] Update typo3/coding-standards to 0.5.x

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
-$config->setCacheFile('.php-cs-fixer.cache');
 $finder = $config->getFinder()
     ->in(__DIR__)
     ->exclude('node_modules')
     ->ignoreVCSIgnored(true);
+
 return $config;
