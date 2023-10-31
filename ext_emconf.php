5f892b311b1cf0520bbf124cc27d93cea7fc2875
Elias Häußler <elias@haeussler.dev>
2023-10-31 21:57:55 +0100

    [TASK] Require at least TYPO3 v12.4.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'elias@haeussler.dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '12.4.2-12.4.99',
             'php' => '8.1.0-8.3.99',
         ],
     ],
