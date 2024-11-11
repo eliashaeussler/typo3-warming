f39308351fb0c0fc29c474c251dcef89e46be530
Elias Häußler <elias@haeussler.dev>
2024-11-11 21:13:19 +0100

    [BUGFIX] Remove default value for `tx_warming_domain_model_log.url`
    Resolves: #729

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tx_warming_domain_model_log (
 	request_id varchar(50) default '' not null,
 	date int(10) unsigned default '0' not null,
-	url text default '' not null,
+	url text not null,
 	message mediumtext,
 	state varchar(20) default '' not null,
 	sitemap text,
